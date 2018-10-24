7cf9864c32c0aee68124bc4aa4bef930fe08bffc
Naya Verdier <nayaverdier@users.noreply.github.com>
2018-10-23 22:54:26 -0700

    Remove unsubscribe from email_list (handled by MailChimp/SendGrid...)

--- registration_2019/email_list.py
+++ registration_2019/email_list.py
@@ -10,8 +10,6 @@ def rand_uuid():
 class EmailSubscription(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    token = db.Column(db.String(36), unique=True, nullable=False, default=rand_uuid)
-    subscribed = db.Column(db.Boolean(), default=True)
 
 def find_sub(search_email):
     return EmailSubscription.query.filter_by(email=search_email).first()
@@ -27,31 +25,8 @@ def subscribe():
         db.session.add(sub)
         db.session.commit()
     else:
-        sub.subscribed = True
         db.session.commit()
     return jsonify({"status": "ok"}), 200
-
-@app.route('/email_list/v1/unsubscribe', methods=['POST'])
-def unsubscribe():
-    json = request.get_json()
-    req_email = json.get('email')
-    req_token = json.get('token')
-
-    sub = find_sub(req_email)
-
-    if sub != None and sub.token == req_token:
-        # email and correct token provided
-        sub.subscribed = False
-        db.session.commit()
-        return jsonify({"status": "ok"}), 200
-    elif sub != None:
-        # incorrect token provided
-        return jsonify({"message": "invalid token"}), 401
-    else:
-        # email not subscribed
-        return jsonify({"status": "ok"}), 200
-
-
 
 # TODO: Needs authentication
 #@app.route('/email_list/v1/subscriptions', methods=['GET'])
